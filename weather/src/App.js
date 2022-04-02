572dbca2170bb7076592ed4b10c09e0411931e1e
Moazam Hussain <moazam694@gmail.com>
2022-04-02 16:53:42 -0700

    pulled from api & added searchbar

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const api = {
+  key: "58b783e3a2f433f05eee3b76c028a9bf",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className='search-box'>
+          <input type="text" className='search-bar' placeholder='Search...'/>
+        </div>
+      </main>
     </div>
   );
 }
