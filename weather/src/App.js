b01531f49589c1a0859f70857825077df9c91d4b
Moazam Hussain <moazam694@gmail.com>
2022-04-04 21:24:59 -0700

    changes made

--- weather/src/App.js
+++ weather/src/App.js
@@ -31,6 +31,12 @@ function App() {
           <div className='location'>New York City, US</div>
           <div className='date'>{dateBuilder(new Date())}</div>
         </div>
+        <div className='weather-box'>
+          <div className='temp'>
+            59°F
+          </div>
+          <div className='weather'>Sunny</div>
+        </div>
       </main>
     </div>
   );
